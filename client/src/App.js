d3e880eda8d1838c7ff41a51cd12036dcbb271a8
Billy <ryotabilly@gmail.com>
2022-07-31 17:55:14 -0700

    implemented SELECT

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Select } from '../src/component/Select';
+
+function App() {
+  return (
+    <div>
+      <Select/>
+    </div>
+  );
+}
+
+export default App;
