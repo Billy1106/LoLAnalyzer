368f93df11ea01e32562d7ab37fac303da0010f6
Billy <ryotabilly@gmail.com>
2022-08-08 21:11:34 -0700

    added react-router

--- client/src/component/NormalUser.jsx
+++ client/src/component/NormalUser.jsx
@@ -9,9 +9,9 @@ export const NormalUser = () => {
         <div className="normaluser">
             <h1>Welcome, normal user.</h1>
             <button onClick={() => navigate("/normaluser/select")}>Select(View)+Projection</button>
-            <button onClick={() => navigate("/")}>Division</button>
+            <button onClick={() => navigate("/riotemployee/division")}>Division</button>
             <button onClick={() => navigate("/normaluser/join")}>Join</button>
-            <button onClick={() => navigate("/")}>Aggregation with Group By</button>
+            <button onClick={() => navigate("/riotemployee/aggregationgroupby")}>Aggregation with Group By</button>
             <button onClick={() => navigate("/riotemployee/aggwithhaving",{state:{action: "Aggregation with having"}})}>Aggregation with having</button>
             <button onClick={() => navigate("/riotemployee/nestedaggwithgb",{state:{action: "Nested aggreagtion with group by"}})}>Nested Aggregation with Group By</button>
             <button onClick={() => navigate("/")}>back</button>
